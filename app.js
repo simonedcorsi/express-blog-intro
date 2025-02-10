7a4ee6ebf2cb176c264260c5737452ab7d4c6595
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-02-10 15:09:18 +0100

    sitemazione

--- app.js
+++ app.js
@@ -12,38 +12,44 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-const posts = [
-    {
-        title: 'Ciambellone',
-        content: 'imaggine ciambellone',
-        image: '/images/ciambellone.jpg',
-        tags: ['ricette', 'piatti tipici']
-      },
-      {
-        title: 'Cracker_barbabietole',
-        content: 'immagine cracker',
-        image: '/images/cracker_barbabietola.jpg',
-        tags: ['ricette', 'piatti tipici']
-      },
-      {
-        title: 'Pane_fritto_dolce',
-        content: 'immagine pane',
-        image: '/images/pane_fritto_dolce.jpg',
-        tags: ['ricette', 'piatti tipici']
-      },
-      {
-        title: 'Pasta_barbabietola',
-        content: 'immagine pasta',
-        image: '/images/pasta_barbabietola.jpg',
-        tags: ['ricette', 'piatti tipici']
-      },
-      {
-        title: 'Torta_paesana',
-        content: 'immagine torta',
-        image: '/images/torta_paesana.jpg',
-        tags: ['ricette', 'piatti tipici']
-      }
-];
+app.get("/api/images", (req, res) => {
+
+    const posts = [
+        {
+            title: 'Ciambellone',
+            content: 'imaggine ciambellone',
+            image: '/images/ciambellone.jpg',
+            tags: ['ricette', 'piatti tipici']
+        },
+        {
+            title: 'Cracker_barbabietole',
+            content: 'immagine cracker',
+            image: '/images/cracker_barbabietola.jpg',
+            tags: ['ricette', 'piatti tipici']
+        },
+        {
+            title: 'Pane_fritto_dolce',
+            content: 'immagine pane',
+            image: '/images/pane_fritto_dolce.jpg',
+            tags: ['ricette', 'piatti tipici']
+        },
+        {
+            title: 'Pasta_barbabietola',
+            content: 'immagine pasta',
+            image: '/images/pasta_barbabietola.jpg',
+            tags: ['ricette', 'piatti tipici']
+        },
+        {
+            title: 'Torta_paesana',
+            content: 'immagine torta',
+            image: '/images/torta_paesana.jpg',
+            tags: ['ricette', 'piatti tipici']
+        }
+    ];
+    res.json(ricette)
+ 
+})    
+    
 
 app.get('/bacheca', (req, res) => {
     res.json(posts);
