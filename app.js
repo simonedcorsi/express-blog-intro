09e279a60056cdf30103d29a921f0c3bee5be9ca
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-02-10 14:53:47 +0100

    creation posts

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog');
 });
@@ -9,3 +11,36 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const posts = [
+    {
+        title: 'Ciambellone',
+        content: 'imaggine ciambellone',
+        image: '/images/ciambellone.jpg',
+        tags: ['ricette', 'piatti tipici']
+      },
+      {
+        title: 'Cracker_barbabietole',
+        content: 'immagine cracker',
+        image: '/images/cracker_barbabietola.jpg',
+        tags: ['ricette', 'piatti tipici']
+      },
+      {
+        title: 'Pane_fritto_dolce',
+        content: 'immagine pane',
+        image: '/images/pane_fritto_dolce.jpg',
+        tags: ['ricette', 'piatti tipici']
+      },
+      {
+        title: 'Pasta_barbabietola',
+        content: 'immagine pasta',
+        image: '/images/pasta_barbabietola.jpg',
+        tags: ['ricette', 'piatti tipici']
+      },
+      {
+        title: 'Torta_paesana',
+        content: 'immagine torta',
+        image: '/images/torta_paesana.jpg',
+        tags: ['ricette', 'piatti tipici']
+      }
+];
